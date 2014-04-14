a8e5273a2fecca04b3be655da9b96e903f7560fd
werne2j <werne2j@cmich.edu>
2014-04-14 12:55:11 -0400

    more time formats allowed

--- management/views.py
+++ management/views.py
@@ -308,7 +308,7 @@ class GameList(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
                 game_form2.save()
                 return HttpResponseRedirect(reverse('game_list', kwargs={'username': request.user.username , 'name': self.kwargs.get("name"), 'year': self.kwargs.get("year")}))
             else:
-                print "errors"
+                print game_form
         return HttpResponseRedirect(reverse('game_list', kwargs={'username': request.user.username , 'name': self.kwargs.get("name"), 'year': self.kwargs.get("year")}))
 
     def get_context_data(self, **kwargs):
